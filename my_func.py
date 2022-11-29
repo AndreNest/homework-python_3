93ed7f44f43c01598fe8d48b1dd143528b917e58
AndreyNesterov <nest.ande@yandex.ru>
2022-11-29 19:33:04 +0300

    complite

--- my_func.py
+++ my_func.py
@@ -8,6 +8,26 @@ def get_int(input_string):
     except(ValueError):
         print('Ошибка ввода! Введите число!')
         return get_int(input_string)
+def get_float(input_string):
+    '''
+    Проверка ввода на число.
+    '''
+    try:
+        num = float(input(input_string))
+        return  num
+    except(ValueError):
+        print('Ошибка ввода! Введите число!')
+        return get_float(input_string)
+def float_num_check(num):
+    num_x = num - round(num)
+    if num_x == 0:
+        print('num')
+        print(f"{int(num)} - число не дробное!")
+    else:
+        print(f"{num} - число дробное!")
+
+float_num_check(get_int('Введите число: '))
+
 def get_str(input_string):
     '''
     Проверка ввода на строку.
--- task_02.py
+++ task_02.py
@@ -8,5 +8,14 @@ def check_float(text):
     except(ValueError):
         print(f'"{text}" - это не дробное число!')
         return check_float(my_func.get_str('Введите строку заного: '))
+def float_num_check(num):
+    num_x = num - round(num)
+    if num_x == 0:
+        print('num')
+        print(f"{int(num)} - число не дробное!")
+    else:
+        print(f"{num} - число дробное!")
 
+float_num_check(my_func.get_float('Введите число: '))
 check_float(my_func.get_str('Введите строку: '))
+
